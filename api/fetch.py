aa4cdda620564e7133b49e7401c2c3887e0e9c3a
Zach <65182259+hydrabeer@users.noreply.github.com>
2024-08-07 16:13:54 -0600

    Improve return type annotation

--- api/fetch.py
+++ api/fetch.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def fetch_2fa_data() -> dict:
+def fetch_2fa_data() -> dict[str, dict]:
     """Fetch data from the 2fa.directory API."""
     url = "https://api.2fa.directory/v4/all.json"
     response = requests.get(url)
