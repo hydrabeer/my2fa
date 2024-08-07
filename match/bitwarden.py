a7a445bef619a5f6c4c46d2c02e2cbbad13cc20c
Zach <65182259+hydrabeer@users.noreply.github.com>
2024-08-07 16:14:35 -0600

    Minimal working matcher

---
+++ match/bitwarden.py
@@ -0,0 +1,18 @@
+import json
+from os import PathLike
+
+
+def bitwarden_items(path: str | PathLike) -> dict[str, list]:
+    output = {}
+    if path[-4:] == "json":
+        with open(path, "r") as f:
+            vault_data = json.load(f)
+            for item in vault_data["items"]:
+                if item.get("login"):
+                    output[item["name"]] = [
+                        entry["uri"] for entry in item["login"]["uris"]
+                    ]
+    else:
+        raise NameError  # cry about it
+
+    return output
---
+++ match/interface.py
@@ -0,0 +1,62 @@
+import urllib.parse
+
+
+class PasswordManagerNotSupportedError(Exception):
+    def __str__(self):
+        return "Sorry, that password manager isn't supported yet."
+
+
+class ExportFileNotRecognizedError(Exception):
+    def __str__(self):
+        return (
+            "Sorry, that export file isn't recognized. Make sure its name contains "
+            "the name of your password manager. If it does, your password manager "
+            "may not be supported yet."
+        )
+
+
+class MatchInterface:
+    """
+    Attributes:
+        _api_data: A dictionary matching the 2fa.directory API v4 format.
+        _password_items_path: The path to the password manager export file.
+        _password_manager: The name of the password manager from which the export file
+                           came.
+    """
+
+    _api_data: dict[str, dict]
+    _password_items_path: str
+    _password_manager: str
+
+    def __init__(self, api_data: dict[str, dict], password_items_path: str) -> None:
+        self._api_data = api_data
+        self._password_items_path = password_items_path
+        self._password_manager = self._detect_password_manager()
+
+    def _detect_password_manager(self) -> str:
+        if "bitwarden" in self._password_items_path.lower():
+            return "bitwarden"
+        else:
+            raise ExportFileNotRecognizedError
+
+    def _get_password_items(self) -> dict[str, list]:
+        match self._password_manager:
+            case "bitwarden":
+                from bitwarden import bitwarden_items
+
+                return bitwarden_items(self._password_items_path)
+
+            case _:
+                raise PasswordManagerNotSupportedError
+
+    def match(self) -> dict:
+        matched_items = {}
+
+        password_items = self._get_password_items()
+        for key in password_items:
+            for uri in password_items[key]:
+                parse_result = urllib.parse.urlparse(uri)
+                if service := self._api_data.get(parse_result.hostname):
+                    matched_items[key] = service.get("methods")
+
+        return matched_items
